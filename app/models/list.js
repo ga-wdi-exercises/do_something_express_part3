f0ebc92fa2bd5cf05d8862ad980f3554e9a70c25
Rachel Warren <raelwa@gmail.com>
2015-08-19 17:18:29 -0400

    sequelize models for Lists and Tasks

--- app/models/list.js
+++ app/models/list.js
@@ -1,5 +1,17 @@
-module.exports = [
-  {id: 1, title:"Errands"},
-  {id: 2, title:"Things that are better than WDI"},
-  {id: 3, title:"WDI To-Dos"}
-]
+module.exports = function(sequelize, Sequelize) {
+  var listOfLists = squelize.define("list", {
+    id: Sequelize.INTEGER,
+    title: Sequelize.STRING,
+  });
+  return listOfLists;
+}
+
+
+//starter code//
+////////////////
+
+// module.exports = [
+//   {id: 1, title:"Errands"},
+//   {id: 2, title:"Things that are better than WDI"},
+//   {id: 3, title:"WDI To-Dos"}
+// ]
--- app/models/task.js
+++ app/models/task.js
@@ -1,5 +1,19 @@
-module.exports = [
-  {id: 1, body:"Water the plants", completed: false, listId: 1},
-  {id: 2, body:"Feed the cat", completed: true, listId: 1},
-  {id: 3, body:"Send the WDI instructors a nice card", completed: false, listId: 3}
-]
+model.exports = function(sequelize, Sequelize) {
+  return sequelize.define("task", {
+    body: Sequelize.STRING,
+    completed: Sequelize.BOOLEAN,
+    listId: Sequelize.INTEGER
+  });
+}
+
+
+
+
+
+//STARTER CODE//
+////////////////
+// module.exports = [
+//   {id: 1, body:"Water the plants", completed: false, listId: 1},
+//   {id: 2, body:"Feed the cat", completed: true, listId: 1},
+//   {id: 3, body:"Send the WDI instructors a nice card", completed: false, listId: 3}
+// ]
