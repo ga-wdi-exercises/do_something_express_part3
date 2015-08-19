ca5677e5d11be833789cc112bd9e657f051373f6
Erica Andersen <andersenek@gmail.com>
2015-08-19 19:03:46 -0400

    Added .gitignore and setup config/connection.js

---
+++ config/connection.js
@@ -0,0 +1,17 @@
+var Sequelize = require("sequelize");
+var sequelize = new Sequelize("postgres:///do_something_db");
+var List = sequelize.import("../models/list");
+var Task = sequelize.import("../models/task");
+
+// Associations have to happen after the models have been defined
+Task.belongsTo(List);
+List.hasMany(Task);
+
+module.exports = {
+  Sequelize: Sequelize,
+  sequelize: sequelize,
+  models: {
+    Task: Task,
+    List: List
+  }
+}
