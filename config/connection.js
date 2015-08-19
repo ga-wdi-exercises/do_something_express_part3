d2c97471f25c37b24a9a636ca7fe07c38b336076
Omari Mayers-Walker <omari@fastmail.fm>
2015-08-19 17:33:34 -0400

    added db

--- config/connection.js
+++ config/connection.js
@@ -4,7 +4,7 @@ var Task = sequelize.import("../app/models/task")
 var List = sequelize.import("../app/models/list")
 
 Task.belongsTo(List)
-List.haMany(Task)
+List.hasMany(Task)
 
 module.exports = {
     Sequelize: Sequelize,
