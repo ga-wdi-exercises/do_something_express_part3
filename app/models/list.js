6cecccf1cdbe4eba9c98cd08053095aed3a907e6
Erica Andersen <andersenek@gmail.com>
2015-08-20 09:01:21 -0400

    Setup config files and adjusted models and seeds

--- app/models/list.js
+++ app/models/list.js
@@ -1,5 +1,18 @@
-module.exports = [
-  {id: 1, title:"Errands"},
-  {id: 2, title:"Things that are better than WDI"},
-  {id: 3, title:"WDI To-Dos"}
-]
+module.exports = function(sequelize, Sequelize){
+  var model = sequelize.define("list", {
+    id: Sequelize.INTEGER,
+    title: Sequelize.STRING
+  },
+  {
+    instanceMethods: {
+      shout: function(){
+        console.log("My name title " + this.title);
+      }
+    }
+  }
+  );
+  model.sing = function(){
+    console.log("Tra la la!");
+  }
+  return model;
+}
