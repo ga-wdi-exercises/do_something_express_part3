b6438567db249699b91d45f6911ae1b8962e55a3
Cameron Fry <cameronfry11@gmail.com>
2015-08-20 09:25:46 -0400

    updated list controller

--- app/controllers/lists.js
+++ app/controllers/lists.js
@@ -10,7 +10,7 @@ function error(response, message){
 
 router.get("/lists", function(req, res){
   List.findAll({order: "id"}).then(function(lists){
-    res.json(lists)
+    res.json(list);
   });
 });
 
@@ -18,35 +18,60 @@ router.post("/lists", function(req, res){
   List.create(req.body).then(function(list){
     res.json(list);
   });
-  return res.json(req.body);
 });
 
 router.get("/lists/:id", function(req, res){
-  for(var l = 0; l < List.length; l++){
-    if(List[l].id == req.params.id){
-      return res.json(List[l]);
-    }
-  }
+
+  List.findById(req.params.id).then(function(list){
+    res.json(list);
+  });
+
+  //Commenting out code from homework 2
+
+  // for(var l = 0; l < List.length; l++){
+  //   if(List[l].id == req.params.id){
+  //     return res.json(List[l]);
+  //   }
+  // }
 });
 
 router.put("/lists/:id", function(req, res){
-  for(var l = 0; l < List.length; l++){
-    if(List[l].id == req.params.id){
-      List[l] = req.body;
-      return res.json(List[l]);
-    }
-  }
-  return error(res, "not found");
+
+  List.findById(req.params.id)
+  .then(function(list{
+    if(!list) return error(res, "not found");
+    return list.updateAttributes(req.body);
+  })
+  .then(function(list){
+    res.json(list);
+  });
+
+  // for(var l = 0; l < List.length; l++){
+  //   if(List[l].id == req.params.id){
+  //     List[l] = req.body;
+  //     return res.json(List[l]);
+  //   }
+  // }
 });
 
 router.delete("/lists/:id", function(req, res){
-  for(var l = 0; l < List.length; l++){
-    if(List[l].id == req.params.id){
-      delete List[l];
-      return res.json(List[l]);
-    }
-  }
-  return error(res, "not found");
+
+  List.findById(req.params.id)
+  .then(function(list){
+    if(!list) return error(res, "not found");
+    return list.destroy()
+  })
+  .then(function(list){
+    res.json(list)
+  });
+
+  // for(var l = 0; l < List.length; l++){
+  //   if(List[l].id == req.params.id){
+  //     delete List[l];
+  //     return res.json(List[l]);
+  //   }
+  // }
+  // return error(res, "not found");
 });
 
 module.exports = router;
