80b9b911de7081c307392f544c7c696dc7685d71
Philip Engles <philipengles@Philips-MacBook-Pro-2.local>
2015-08-19 20:45:45 -0400

    HW on expressjs

---
+++ config/seeds.js
@@ -0,0 +1,26 @@
+var DB = require("./connection");
+var Seeds = {
+  lists: require("./list_data"),
+  tasks: require("./task_data")
+}
+
+DB.models.Movie.bulkCreate(Seeds.lists)
+.then(function(){
+  return DB.models.Movie.findAll();
+})
+.then(function(lists){
+  var a, list, s, task, tasks, output = [];
+  for(a = 0; a < lists.length; a++){
+    list = lists[a];
+    tasks = Seeds.tasks[list.name];
+    for(s = 0; s < tasks.length; s++){
+      task = tasks[s];
+      task.listId = list.id;
+      output.push(task);
+    }
+  }
+  return DB.models.Task.bulkCreate(output);
+})
+.then(function(){
+  process.exit();
+});
