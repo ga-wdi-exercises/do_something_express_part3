5af3525b21ec447cf438598081455af1c4b8177f
Cameron Fry <cameronfry11@gmail.com>
2015-08-20 17:14:57 -0400

    s

--- app/controllers/tasks.js
+++ app/controllers/tasks.js
@@ -16,57 +16,30 @@ router.get("/tasks", function(req, res){
 });
 
 router.get("/tasks/:id", function(req, res){
-
   Task.findById(req.params.taskId).then(function(task){
     res.json(task);
   })
-
-  // for(var t = 0; t < Task.length; t++){
-  //   if(Task[t].id == req.params.id){
-  //     return res.json(Task[t]);
-  //   }
-  // }
-  // return error(res, "not found");
 });
 
 router.put("/tasks/:id", function(req, res){
-
   Task.findById(req.params.taskId).then(function(taks){
     if(!task) return error(res, "not found");
     task.updateAttributes(req.body).then(function(taks){
       res.json(task);
     });
   });
-
-  // for(var t = 0; t < Task.length; t++){
-  //   if(Task[t].id == req.params.id){
-  //     Task[t] = req.body;
-  //     return res.json(Task[t]);
-  //   }
-  // }
-  // return error(res, "not found");
 });
 
 router.delete("/tasks/:id", function(req, res){
-
   Task.findById(req.params.taskId).then(function(task){
     if(!task) return error(res, "not found");
     task.destroy().then(function(db_res){
       res.json(db_res);
     });
   });
-
-  // for(var t = 0; t < Task.length; t++){
-  //   if(Task[t].id == req.params.id){
-  //     delete Task[t];
-  //     return res.json({"success": true});
-  //   }
-  // }
-  // return error(res, "not found");
 });
 
 router.get("/lists/:listId/tasks", function(req, res){
-
   List.findById(req.params.listId)
   .then(function(list){
     if(!list) return error(res, "not found");
@@ -75,18 +48,9 @@ router.get("/lists/:listId/tasks", function(req, res){
   .then(function(tasks){
     res.json(tasks);
   });
-
-  // var tasks = [];
-  // for(var t = 0; t < Task.length; t++){
-  //   if(Task[t].listId == req.params.listId){
-  //     tasks.push(Task[t]);
-  //   }
-  // }
-  // return res.json(tasks);
 });
 
 router.post("/lists/:listId/tasks", function(req, res){
-
   List.findById(req.params.listId)
   .then(function(list){
     if(!list) return error(res, "not found");
@@ -94,18 +58,7 @@ router.post("/lists/:listId/tasks", function(req, res){
   })
   .then(function(tasks){
     res.json(tasks);
-  }); 
-
-  // var task;
-  // for(var l = 0; l < List.length; l++){
-  //   if(List[l].id == req.params.listId){
-  //     task = req.body;
-  //     task.listId = req.params.listId;
-  //     Task.push(task);
-  //     return res.json(task);
-  //   }
-  // }
-  // return error(res, "not found");
+  });
 });
 
 module.exports = router;
--- app/models/list.js
+++ app/models/list.js
@@ -1,9 +1,3 @@
-// module.exports = [
-//   {id: 1, title:"Errands"},
-//   {id: 2, title:"Things that are better than WDI"},
-//   {id: 3, title:"WDI To-Dos"}
-// ]
-
 module.exports = function(sequelize, DataTypes){
   return sequelize.define("list", {
     title: DataTypes.STRING,
