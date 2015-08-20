ff7f5d7627a494b02b5a2334b5a5ece705224ea3
Erica Irving <contact.eirving@gmail.com>
2015-08-20 17:56:59 -0400

    create connection.js

---
+++ db/connection.js
@@ -0,0 +1,16 @@
+var Sequelize = require("sequelize");
+var sequelize = new Sequelize("postgres:///do_something_db");
+var List = sequelize.import("../app/models/list.js");
+var Task = sequelize.import("../app/models/task.js");
+
+List.hasMany(Task);
+Task.belongsTo(List);
+
+module.exports = {
+  Sequelize: Sequelize,
+  sequelize: sequelize,
+  models: {
+    List: List,
+    Artist: Artist
+  }
+};
