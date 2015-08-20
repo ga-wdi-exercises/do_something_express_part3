cb66614cef24bba776bb50c7ce1ce9aa53e3a950
Erica Irving <contact.eirving@gmail.com>
2015-08-20 18:04:29 -0400

    convert models to Sequelize

--- app/models/task.js
+++ app/models/task.js
@@ -1,5 +1,7 @@
-module.exports = [
-  {id: 1, body:"Water the plants", completed: false, listId: 1},
-  {id: 2, body:"Feed the cat", completed: true, listId: 1},
-  {id: 3, body:"Send the WDI instructors a nice card", completed: false, listId: 3}
-]
+module.exports = function (sequelize, Sequelize) {
+  var model = sequelize.define("task", {
+    body: Sequelize.STRING,
+    completed: Sequelize.BOOLEAN
+  });
+  return model;
+};
---
+++ db/seeds.js
@@ -0,0 +1,10 @@
+{id: 1, title:"Errands"},
+{id: 2, title:"Things that are better than WDI"},
+{id: 3, title:"WDI To-Dos"}
+]
+
+[
+  {id: 1, body:"Water the plants", completed: false, listId: 1},
+  {id: 2, body:"Feed the cat", completed: true, listId: 1},
+  {id: 3, body:"Send the WDI instructors a nice card", completed: false, listId: 3}
+]
