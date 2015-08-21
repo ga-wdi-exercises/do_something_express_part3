f634ad662ccf0d7c82efae15d818e634540b6af7
Omari Mayers-Walker <omari@fastmail.fm>
2015-08-21 08:44:05 -0400

    latest

--- app/models/list.js
+++ app/models/list.js
@@ -1,17 +1,6 @@
-module.exports = [{
-    id: 1,
-    title: "Errands"
-}, {
-    id: 2,
-    title: "Things that are better than WDI"
-}, {
-    id: 3,
-    title: "WDI To-Dos"
-}]
-
 module.exports = function(sequelize, Sequelize) {
     var model = sequelize.define("list", {
         title: Sequelize.STRING
-    }, {});
-    return model;
+    }, {})
+    return model
 }
