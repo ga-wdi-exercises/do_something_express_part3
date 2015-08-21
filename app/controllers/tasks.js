3d5bb642cd1af34d1bda24df558a03fc383ab03b
Jeremy Koulish <jkoulish@gmail.com>
2015-08-21 10:59:37 -0400

    updated connection, models & controllers

--- app/controllers/tasks.js
+++ app/controllers/tasks.js
@@ -1,67 +1,54 @@
 var express = require("express");
 var router = express.Router();
-var Task = require("../models/task");
-var List = require("../models/list");
+var Task = require("../db/connection").models.Task;
+
 
 function error(response, message){
   response.status(500);
   response.json({error: message})
 }
 
+router.get("/lists/:listId/tasks", function(req, res){
+  List.findById(req.params.listId).then(function(list){
+    res.json(list.getTasks())
+  })
+})
+
 router.get("/tasks", function(req, res){
-  return res.json(Task);
+  Task.findAll().then(function(tasks){
+    res.json(tasks);
+  });
+});
+
+router.post("/tasks", function(req, res){
+  Task.create(req.body).then(function(task){
+    res.json(task);
+  });
 });
 
 router.get("/tasks/:id", function(req, res){
-  for(var t = 0; t < Task.length; t++){
-    if(Task[t].id == req.params.id){
-      return res.json(Task[t]);
-    }
-  }
-  return error(res, "not found");
+  Task.findById(req.params.id).then(function(task){
+    if(!task) return error(res, "not found");
+    res.json(task);
+  });
 });
 
-router.put("/tasks/:id", function(req, res){
-  for(var t = 0; t < Task.length; t++){
-    if(Task[t].id == req.params.id){
-      Task[t] = req.body;
-      return res.json(Task[t]);
-    }
-  }
-  return error(res, "not found");
+router.patch("/tasks/:id", function(req, res){
+  Task.findById(req.params.id).then(function(task){
+    if(!task) return error(res, "not found");
+    task.updateAttributes(req.body).then(function(updatedTask){
+      res.json(updatedTask);
+    });
+  });
 });
 
 router.delete("/tasks/:id", function(req, res){
-  for(var t = 0; t < Task.length; t++){
-    if(Task[t].id == req.params.id){
-      delete Task[t];
-      return res.json({"success": true});
-    }
-  }
-  return error(res, "not found");
-});
-
-router.get("/lists/:listId/tasks", function(req, res){
-  var tasks = [];
-  for(var t = 0; t < Task.length; t++){
-    if(Task[t].listId == req.params.listId){
-      tasks.push(Task[t]);
-    }
-  }
-  return res.json(tasks);
-});
-
-router.post("/lists/:listId/tasks", function(req, res){
-  var task;
-  for(var l = 0; l < List.length; l++){
-    if(List[l].id == req.params.listId){
-      task = req.body;
-      task.listId = req.params.listId;
-      Task.push(task);
-      return res.json(task);
-    }
-  }
-  return error(res, "not found");
+  Task.findById(req.params.id).then(function(task){
+    if(!task) return error(res, "not found");
+    task.destroy().then(function(){
+      res.json({success: true});
+    });
+  });
 });
 
 module.exports = router;
